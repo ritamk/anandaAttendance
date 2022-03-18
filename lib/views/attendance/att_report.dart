d76e09fa4dd190617c6d1f24a3da9bd85fc9d1d8
ritamk <ritamkonar7@gmail.com>
2022-03-18 10:55:10 +0530

    changes to UI

--- lib/views/attendance/att_report.dart
+++ lib/views/attendance/att_report.dart
@@ -56,21 +56,27 @@ class AttReportStatePage extends State<AttReportPage> {
                           ? ListView.builder(
                               itemCount: snapshot.data!.length,
                               itemBuilder: (BuildContext context, int index) {
-                                return ListTile(
-                                  title: Text(
-                                      DateFormat.Hms()
-                                          .format(snapshot.data![index]["time"]
-                                              .toDate())
-                                          .toString(),
-                                      style: const TextStyle(
-                                          fontSize: 16.0,
-                                          fontWeight: FontWeight.bold)),
-                                  leading: snapshot.data![index]["reporting"]
-                                      ? Icon(Icons.login_rounded,
-                                          color: Colors.green.shade800)
-                                      : Icon(Icons.logout_rounded,
-                                          color: Colors.red.shade800),
-                                );
+                                return !snapshot.data![index]["leave"]
+                                    ? ListTile(
+                                        title: Text(
+                                            DateFormat.Hms()
+                                                .format(snapshot.data![index]
+                                                        ["time"]
+                                                    .toDate())
+                                                .toString(),
+                                            style: const TextStyle(
+                                                fontSize: 16.0,
+                                                fontWeight: FontWeight.bold)),
+                                        leading: snapshot.data![index]
+                                                ["reporting"]
+                                            ? Icon(Icons.login_rounded,
+                                                color: Colors.green.shade800)
+                                            : Icon(Icons.logout_rounded,
+                                                color: Colors.red.shade800),
+                                      )
+                                    : const ListTile(
+                                        title: Text("On leave."),
+                                        leading: Icon(Icons.check));
                               },
                               padding: const EdgeInsets.all(16.0),
                               shrinkWrap: true,
