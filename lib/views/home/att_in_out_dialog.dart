c3de3de9222d7fa2b2bd188f75b20e01ade50662
ritamk <ritamkonar7@gmail.com>
2022-03-22 23:45:01 +0530

    added packages and changed UX in attendance

--- lib/views/home/att_in_out_dialog.dart
+++ lib/views/home/att_in_out_dialog.dart
@@ -1,9 +1,13 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:face_rec/models/attendance_model.dart';
+import 'package:face_rec/services/database.dart';
+import 'package:face_rec/shared/snackbar.dart';
 import 'package:face_rec/views/attendance/attendance.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 
-class AttendanceInOrOutDialog extends StatelessWidget {
+class AttendanceInOrOutDialog extends StatefulWidget {
   const AttendanceInOrOutDialog({
     Key? key,
     required this.uid,
@@ -13,21 +17,76 @@ class AttendanceInOrOutDialog extends StatelessWidget {
   final GeoPoint loc;
 
   @override
+  State<AttendanceInOrOutDialog> createState() =>
+      _AttendanceInOrOutDialogState();
+}
+
+class _AttendanceInOrOutDialogState extends State<AttendanceInOrOutDialog> {
+  bool loading = false;
+  Position? coord;
+  GeoPoint? geoPointCoord;
+  GeoPoint? desiredCoord;
+  final double coordVar = 0.0005;
+  bool verified = false;
+
+  @override
   Widget build(BuildContext context) {
     return CupertinoAlertDialog(
-      title: const Text("Attendance type"),
-      content:
+      title: const Text("Verify geolocation"),
+      content: Column(
+        children: <Widget>[
+          MaterialButton(
+            child:
+                const Icon(Icons.location_on, size: 24.0, color: Colors.blue),
+            onPressed: () async {
+              setState(() => loading = true);
+              try {
+                coord = await _determinePosition();
+                geoPointCoord = GeoPoint(coord!.latitude, coord!.longitude);
+                desiredCoord = widget.loc;
+                if ((geoPointCoord!.latitude - desiredCoord!.latitude).abs() <
+                        coordVar &&
+                    (geoPointCoord!.longitude - desiredCoord!.longitude).abs() <
+                        coordVar) {
+                  setState(() {
+                    loading = false;
+                    commonSnackbar(
+                        "Geolocation verification successful.", context);
+                    verified = true;
+                  });
+                } else {
+                  setState(() {
+                    loading = false;
+                    commonSnackbar("Geolocation verification failed.", context);
+                  });
+                }
+              } catch (e) {
+                setState(() {
+                  loading = false;
+                  commonSnackbar(e.toString(), context);
+                });
+              }
+            },
+            highlightColor: Colors.transparent,
+            splashColor: Colors.transparent,
+          ),
           const Text("Are you entering (In) or leaving (Out) the location?"),
+        ],
+      ),
       actions: <Widget>[
         CupertinoDialogAction(
           textStyle: const TextStyle(color: Colors.blue, fontSize: 16.0),
           child: const Text("In"),
-          onPressed: () => toAttPage(context, true),
+          onPressed: () => verified
+              ? toAttPage(context, true)
+              : commonSnackbar("Geolocation not verified", context),
         ),
         CupertinoDialogAction(
           textStyle: const TextStyle(color: Colors.blue, fontSize: 16.0),
           child: const Text("Out"),
-          onPressed: () => toAttPage(context, false),
+          onPressed: () => verified
+              ? toAttPage(context, false)
+              : commonSnackbar("Geolocation not verified", context),
         ),
       ],
     );
@@ -36,7 +95,37 @@ class AttendanceInOrOutDialog extends StatelessWidget {
   Future toAttPage(BuildContext context, bool reporting) {
     Navigator.pop(context);
     return Navigator.of(context).push(CupertinoPageRoute(
-        builder: (context) =>
-            AttendancePage(uid: uid, loc: loc, reporting: reporting)));
+        builder: (context) => AttendancePage(
+            uid: widget.uid, loc: widget.loc, reporting: reporting)));
+  }
+
+  Future<Position?> _determinePosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      return Future.error('Please enable location services.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return Future.error('Please allow location permissions.');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      return Future.error(
+          'Cannot request permissions, location permissions are permanently denied.');
+    }
+
+    try {
+      return await Geolocator.getCurrentPosition(
+          desiredAccuracy: LocationAccuracy.high);
+    } catch (e) {
+      return Geolocator.getLastKnownPosition();
+    }
   }
 }
